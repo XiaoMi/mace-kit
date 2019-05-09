bab3fd25c278ff4414e9ce9734911eec29fc7d74
liyin <liyin@xiaomi.com>
2019-05-09 19:21:17 +0800

    Refactor bbox public interface

--- src/util/ssd_bbox.h
+++ src/util/ssd_bbox.h
@@ -18,22 +18,20 @@
 #include <cstdint>
 #include <vector>
 
+namespace mace_kit {
+namespace util {
+
 class SSDBbox {
  public:
   SSDBbox();
   ~SSDBbox();
 
   // Decode single feature layer localization regression output.
-  // This pixel may relate to several bboxes according to anchor size and
-  // anchor ratio set during training.
-  // `feature_anchor_count` equals to  number of pixels times number of anchors
-  // for each pixel. Say, a feature layer is 10x10, and anchor count per
-  // feature is 5, `feature_anchor_count` should be 10x10x5=500.
-  // `anchor_localization` is localization of reference anchors generated
-  // which should be kept consistent with values during training. Its size
-  // should be `feature_anchor_count` x 4.
   // `feature_localization` is regressed output localization. Its size
-  // should be `feature_localization` x 4.
+  // should be `feature_size` x anchor_count x 4 (cy, cx, h, w).
+  // `feature_size` equals to number of pixels for this feature layer.
+  // `anchor_shapes` contains all the shape (height, width) of different
+  // anchors according to anchor size and anchor ratio set during training.
   // `prior_scaling` is hyperparameter for encode width and height of bboxes.
   //
   // Encoding formula:
@@ -56,21 +54,17 @@ class SSDBbox {
   //
   // Note: feature_localization should be both input and output,
   // and output should be clipped within [0, 1]
-  // localization should be in the form of cy, cx, h, w.
-  void Decode(const float *anchor_localization,
-              float *feature_localization,
-              int64_t feature_anchor_count,
+  //
+  void Decode(float *feature_localization,
+              int64_t feature_size,
+              const std::vector<std::vector<float>> &anchor_shapes,
               const std::vector<float> &prior_scaling);
 
-  // Generate anchor bboxes for a feature layer.
-  // output localization should be in the form of cy, cx, h, w.
-  void GenerateAnchors(int64_t img_height, int64_t img_width,
-                       int64_t feature_height, int64_t feature_width,
-                       int64_t step_height, int64_t step_width,
+  // Get anchor bboxes height and with for a feature layer.
+  void GetAnchorsShape(int64_t img_height, int64_t img_width,
                        int64_t min_size, int64_t max_size,
                        const std::vector<float> &anchor_ratios,
-                       float offset,
-                       float *anchor_localization);
+                       std::vector<std::vector<float>> *anchor_shapes);
 
   // First, select predicted bboxes with top-k scores,
   // second, do nms.
@@ -85,5 +79,8 @@ class SSDBbox {
   float CalJaccard(const std::vector<float> &lhs,
                    const std::vector<float> &rhs);
 };
+
+}  // namespace util
+}  // namespace mace_kit
 
 #endif  // MACEKIT_SRC_UTIL_SSD_BBOX_H_
