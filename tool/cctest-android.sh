d6eab03d82c67187984ed0a536caba79e11bcf0d
liutuo <liutuo@xiaomi.com>
2019-12-13 19:28:29 +0800

    fix adb copy

--- tool/cctest-android.sh
+++ tool/cctest-android.sh
@@ -17,7 +17,7 @@ for serialno in ${array[@]}; do
     adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data
     adb -s $serialno push data/asr-tiny /data/local/tmp/macekit/data/
     adb -s $serialno push data/test /data/local/tmp/macekit/data/
-    adb -s $serialno push ${ANDROID_NDK_HOME}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so /data/local/tmp/macekit/lib/
+    adb -s $serialno push ${ANDROID_NDK_HOME}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so /data/local/tmp/macekit/lib/libc++_shared.so
 
     adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/face_detection_unitest"
     adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/face_recognition_unitest"
