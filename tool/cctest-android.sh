1427e5f25af36d5fc24acfd0a2bd56288215228f
liutuo <liutuo@xiaomi.com>
2019-12-18 18:21:19 +0800

    fix ci and update models' libs

--- tool/cctest-android.sh
+++ tool/cctest-android.sh
@@ -10,17 +10,38 @@ while [ "$i" -lt "$length" ];do
     fi
     ((i++))
 done
+ERRORS=("Aborted" "FAILED" "CANNOT LINK EXECUTABLE" "Segmentation fault")
+function check_err() {
+  echo "$1"
+  for i in "${ERRORS[@]}"; do
+  if  grep -q "$i" <<< "$1"; then
+    echo "Test Failed!"
+    exit 1
+  fi
+  done
+}
 for serialno in ${array[@]}; do
-    adb -s $serialno shell mkdir -p /data/local/tmp/macekit
-    adb -s $serialno push cmake-build/android-armeabi-v7a/install/lib /data/local/tmp/macekit/
-    adb -s $serialno push cmake-build/android-armeabi-v7a/install/bin /data/local/tmp/macekit/
-    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data
-    adb -s $serialno push data/asr-tiny /data/local/tmp/macekit/data/
-    adb -s $serialno push data/test /data/local/tmp/macekit/data/
+    echo "========== Test on device(${serialno}) ==========="
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/lib
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/bin
+    adb -s $serialno push cmake-build/android-armeabi-v7a/install/lib/. /data/local/tmp/macekit/lib
+    adb -s $serialno push cmake-build/android-armeabi-v7a/install/bin/. /data/local/tmp/macekit/bin
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data/asr-tiny/online/conf
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data/asr-tiny/online/ivector_extractor
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data/asr-tiny/model
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data/asr-tiny/graph_tiny/phones
+    adb -s $serialno shell mkdir -p /data/local/tmp/macekit/data/test
+    adb -s $serialno push data/asr-tiny/. /data/local/tmp/macekit/data/asr-tiny
+    adb -s $serialno push data/test/. /data/local/tmp/macekit/data/test
     adb -s $serialno push ${ANDROID_NDK_HOME}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so /data/local/tmp/macekit/lib/libc++_shared.so
 
-    adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/face_detection_unitest"
-    adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/face_recognition_unitest"
-    adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/speech_recognition_unitest"
-    adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/object_detection_unitest"
+    OUTPUT=$(adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/face_detection_unitest")
+    check_err "$OUTPUT"
+    OUTPUT=$(adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/face_recognition_unitest")
+    check_err "$OUTPUT"
+    OUTPUT=$(adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/speech_recognition_unitest")
+    check_err "$OUTPUT"
+    OUTPUT=$(adb -s $serialno shell "cd /data/local/tmp/macekit; LD_LIBRARY_PATH=lib ./bin/object_detection_unitest")
+    check_err "$OUTPUT"
+    adb -s $serialno shell "cd /data/local/tmp; rm -rf macekit"
 done
